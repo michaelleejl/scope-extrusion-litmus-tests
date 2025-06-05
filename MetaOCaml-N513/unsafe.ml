9fc09c9add41b17ed076463eefd9f8c74c65644f
michaelleejl <michaelleejl@gmail.com>
2025-06-05 18:03:27 +0100

    add metaocaml tests

---
+++ MetaOCaml-N513/unsafe.ml
@@ -0,0 +1,16 @@
+open Effect
+open Effect.Deep
+open Runcode 
+type _ Effect.t += Extrude: int code -> int code t
+
+let () =  Trx.set_with_stack_mark
+    {Trx.stackmark_region_fn =
+        fun body ->
+          let module M = struct type _ Effect.t += E: unit t end in
+          try body (fun () -> try perform M.E; true with Unhandled _ -> false)
+          with effect M.E, k -> continue k ()};;
+          
+run (let z  = match .<fun x -> .~(perform (Extrude .<x>.) ) >. 
+             with u -> .<0>.
+                | effect (Extrude y), k -> y
+            in .< .~z + 1 >.) ;;
